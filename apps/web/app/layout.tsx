22e860ac8c45a429e5d6dcf8c1c4652fce26d2b7
Soumalya <geniussantu1983@gmail.com>
2026-07-25 20:41:01 +0530

    add 1024x1024 PNG favicon for high-res displays, keep ICO as fallback

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192.svg" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="1024x1024" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
